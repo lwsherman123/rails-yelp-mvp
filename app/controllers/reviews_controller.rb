58920511a0164bcb9215bc3716323c113821dda6
Luke Sherman <lwsherman123@gmail.com>
2023-02-22 14:34:23 +0000

    finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant, only: %i[new create]
-
-  def new
-    @review = Review.new
-  end
+  before_action :set_restaurant, only: [:create]
 
   def create
     @review = Review.new(review_params)
@@ -13,11 +9,10 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
-private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
